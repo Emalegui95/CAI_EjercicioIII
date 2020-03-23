7fb4eb0f42018234145f2c87366153899f0b189c
Emmanuel Leguizamon <62218305+Emalegui95@users.noreply.github.com>
2020-03-23 19:57:05 -0300

    Ejercicio III

---
+++ CAI_EjercicioIII/CAI_EjercicioIII/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_EjercicioIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Ingrese una palabra: ");
+            string palabra = Console.ReadLine();
+            int incr = 0;
+            int decre = palabra.Length - 1;
+            bool esError = false;
+
+            while ((incr < decre) && (!esError))
+            {
+                if (palabra[incr] == palabra[decre])
+                {
+                    incr++;
+                    decre--;
+                }
+                else
+                {
+                    esError = true;
+                }
+
+            }
+
+            if (!esError)
+            {
+                Console.WriteLine("La palabra " + palabra + " es un palindromo");
+            }
+            else
+            {
+                Console.WriteLine("La palabra " + palabra + " no es un palindromo");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
